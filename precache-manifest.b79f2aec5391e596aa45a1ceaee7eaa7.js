0c59cc457186fe9334506169ce9db7f1cd49b7a3
Aleksandr <a.kravchenko.intek@gmail.com>
2020-06-30 14:20:53 +0200

    Updates

---
+++ precache-manifest.b79f2aec5391e596aa45a1ceaee7eaa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e1af314e05089085c250815bf8ab8d",
+    "url": "/monster-sandbox/index.html"
+  },
+  {
+    "revision": "72b35357a3f4e731da71",
+    "url": "/monster-sandbox/static/css/main.aaf71fb3.chunk.css"
+  },
+  {
+    "revision": "107330ef7d6777b46847",
+    "url": "/monster-sandbox/static/js/2.eca49929.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-sandbox/static/js/2.eca49929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72b35357a3f4e731da71",
+    "url": "/monster-sandbox/static/js/main.4656968c.chunk.js"
+  },
+  {
+    "revision": "9e059b2b35bd798150fa",
+    "url": "/monster-sandbox/static/js/runtime-main.2020b755.js"
+  }
+]);
